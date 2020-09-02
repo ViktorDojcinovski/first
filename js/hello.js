346a70390743def710b511932a1f6889ad43ebd1
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-09-02 19:54:22 +0200

    Expresions and statements

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,45 @@
-let hello = "Hello World";
+/* Variable declaration statement */
+let hello;
+let result;
+let number = 20;
+hello = "Hello World 2020";
 
+15 + 15; // 30
+
+"Hello " + "World"; // "Hello World"
+
+let bolleanVariable = true;
+let bolleanVariableF = false;
+
+result = 0;
+console.log("Rezultatot e: " + result);
+
+/* Function declaration statement */
+function zbirOdDvaBroja(a, b) {
+  result = a + b;
+}
+
+zbir(number, 5);
+console.log("Rezultatot e: " + result);
+
+/*
+ varible o will have similar structure to this one:
+
+ {
+     a: 1,
+     b: 2,
+     hello: "Hello World" ...
+ }
+*/
+let o = new Object();
+
+/*
+ varibale a will have similar structure to this one:
+
+ ["1", 5, true, "Hello World"]
+*/
+let a = new Array();
 /* Find the h1 tag inside index.html */
-let h1_hello = document.querySelector("h1");
+let h1_hello = document.querySelector("");
 
 h1_hello.textContent = hello;
