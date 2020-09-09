3ba0b4d35990128d8a43540f1903acd9ed2b813a
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-09-09 20:08:56 +0200

    loops finish

--- js/loops.js
+++ js/loops.js
@@ -1,10 +1,28 @@
 /* Write a for loop that will fill an array with the value of the iterating variable
  */
+let a = [];
 
-/* Write for in loop for an object nested in another object 
+for (let i = 0; i < 10; i++) {
+  a[i] = i;
+}
 
-*/
+console.log(a);
 
+/* Write for in loop for an object nested in another object
+ */
+let person = {
+  name: "Viktorija",
+  lastname: "Loba",
+  address: {
+    street: "Partizanska",
+    number: "30",
+    city: "Skopje",
+  },
+};
+
+for (let n in person.address) {
+  console.log(n);
+}
 /* Write for in loop for the following object {a: 1, b: 2, c: 3} that will log the following:
 "a is 1" "b is 2" "c is 3"
 */
@@ -12,3 +30,15 @@
 /* Write a for loop that will fill an array with anonimous functions and than another loop
 that will execute those functions. In the first loop use the iterating value inside the anonimous functions.
 */
+
+let arr = [];
+
+for (var i = 0; i < 3; i++) {
+  arr[i] = function () {
+    return i;
+  };
+}
+
+for (let j = 0; j < 3; j++) {
+  console.log(arr[j]());
+}
