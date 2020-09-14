cebefe650ac6771edc52687b2c981d13e00d9b11
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-09-14 15:01:14 +0200

    arrays start

---
+++ js/arrays.js
@@ -0,0 +1,9 @@
+/* Create a menu-like structure inside the current project with the help of the forEach array method.
+During the creation, callback function should check if any of the array element is empty, and if it is
+should skip it. The menu array itself does not hold the "home" and "contact" items, which should appear at the
+start and at the end position of the menu like structure, so they have to be added programmaticaly.
+*/
+
+// let menu = "";
+// let arr = ["about", "services"];
+// let nav = document.getElementById("main-menu");
