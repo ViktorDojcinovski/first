6c58deb451dee2e3c3d43c456423c028466858f3
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-10-05 18:48:20 +0200

    last commit

--- js/ajax.js
+++ js/ajax.js
@@ -2,28 +2,63 @@
  * Basic call using XHR (legacy code)
  */
 
+// let button = document.getElementById("btn");
+
+// button.onclick = function () {
+//   loadDoc();
+// };
+
 // function loadDoc() {
 //   var xhttp = new XMLHttpRequest();
 //   xhttp.onreadystatechange = function () {
 //     if (this.readyState == 4 && this.status == 200) {
-//       //document.getElementById("demo").innerHTML = this.responseText;
-//       console.log(this.responseText);
+//       let ul = document.getElementById("todos");
+
+//       for (let todo of JSON.parse(this.responseText)) {
+//         let li = document.createElement("li");
+
+//         li.innerHTML = todo.title;
+
+//         ul.appendChild(li);
+//       }
 //     }
 //   };
-//   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+
+//   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
 //   xhttp.send();
 // }
-
-// loadDoc();
 
 /**
  * Ajax with fetch API
  */
 
-// function loadDocWithFetch() {
-//   return fetch("https://jsonplaceholder.typicode.com/todos/1");
-// }
+function loadDocWithFetch() {
+  return fetch("https://jsonplaceholder.typicode.com/todos");
+}
 
-// let res = loadDocWithFetch();
+let result = loadDocWithFetch();
 
-// res.then((res) => res.json()).then((data) => console.log(data));
+console.log(result instanceof Promise);
+
+result
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (data) {
+    let ul = document.getElementById("todos");
+
+    for (let todo of data) {
+      let li = document.createElement("li");
+
+      li.innerHTML = todo.title;
+
+      ul.appendChild(li);
+    }
+  })
+  .catch(function (err) {
+    console.log("an error occured");
+    console.log(err);
+  })
+  .finally(function () {
+    console.log("Finish the code");
+  });
