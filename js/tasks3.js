eacdc500ef0ed20f17e9d152b6c48de9b2862c34
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-11-27 18:04:14 +0100

    Add tasks for JavaScript

---
+++ js/tasks3.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * 1. Write a function that for a given string returns the character that repeats the most.
+ *
+ * Possible solution:
+ * Within a function (try not to use global variables):
+ * Define and Initialize an object
+ * Map character count to that object.
+ * Loop through the object to find the max count.
+ * Assign new max count value to maxCharCount and new max key to maxChar.
+ * Return maxChar
+ *
+ * Example: countMostOccurances("I am a Codeacademy student.") --> returns "a"
+ */
+
+/**
+ * 2. Write a function that returns the longest string in an array
+ * (with AND withot ES6 syntax - means that you need to provide two solutions)
+ *
+ * Possible solution:
+ * Initialize and Declare Variable for Longest String
+ * Loop through the array
+ * Find the length of each string
+ * Determine if that length is the largest we have seen so far.
+ * Replace the longest/largest variable with the arr[index] as long as length is greater than the variable.
+ * Return longest/largest string.
+ *
+ * Example: findLongest("I am a Codeacademy student.") --> returns "Codeacademy"
+ */
+
+/**
+ * Bonus task
+ *
+ * 3. Write a function that can receive a string as a parametar that possibly contains three types of braces ({}, [], ()),
+ * and returns a Boolean indicating whether the given string contains a valid nesting of braces (valid means, as many closed as opened).
+ *
+ * Example: isBalanced("function () { console.log()")
+ */
